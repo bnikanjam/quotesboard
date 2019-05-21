a1ead278f4e9f445c1260d03440e93aafcafc2ed
Babak <babak.nikanjam@gmail.com>
2019-05-20 19:01:01 -0700

    chore: Code TODOs
    Begin urls and views
    Make and migrate 1st model (db table)

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Quote(models.Model):
+    quote = models.TextField()
+    author = models.CharField(max_length=128)
+    source = models.URLField(blank=True, null=True)
+    cover = models.URLField(blank=True, null=True)
+    added = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.quote} - {self.author}'
+
+    class Meta:
+        ordering = ['-added']
