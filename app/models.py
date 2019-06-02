651417a256d63c6638820d913445d2ce92d4dc0a
Babak <babak.nikanjam@gmail.com>
2019-06-01 17:25:35 -0700

    feat: Model & generate db ORM
    Fixed DB credentials config.sh
    Fixed DB ENGINE setting

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Quotes(models.Model):
+    quote_txt = models.TextField()
+    quote_img = models.URLField(blank=True, null=True)
+    author = models.CharField(max_length=50, default='Anonymous', null=True)
+    source = models.URLField(blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'"{self.quote_txt}" by -{self.author}-'
+
+    class Meta():
+        ordering = ['-date_added']
--- website/settings.py
+++ website/settings.py
@@ -57,7 +57,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('NAME'),
         'USER': os.environ.get('USER'),
         'PASSWORD': os.environ.get('PASSWORD'),
