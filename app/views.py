57b07a1cbf90419f2080848c79dde9d282fd4a77
Babak <babak.nikanjam@gmail.com>
2019-06-02 14:39:48 -0700

    feat: Views and templates
    Add functional views
    Add app's views templates

--- app/views.py
+++ app/views.py
@@ -11,20 +11,42 @@ from .forms import QuoteForm
 
 def show_all(request):
     quotes = Quote.objects.all()
-    return render(request, 'app/show_all.html', {'quotes': quotes})
+    return render(request, 'app/show_all.html', {'app': quotes})
 
 
 def show_one(request, pk):
-    pass
+    one_quote = get_object_or_404(Quote, pk=pk)
+    return render(request, 'app/show_one.html', {'quote': one_quote})
 
 
 def new(request):
-    pass
+    form = QuoteForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'One quote just got added')
+        return redirect('app:show_all')
+    else:
+        return render(request, 'app/quote_form.html', {'form': form})
 
 
 def edit(request, pk):
-    pass
+    to_edit = get_object_or_404(Quote, pk=pk)
+    form = QuoteForm(request.POST or None, instance=to_edit)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, f'Updated')
+        return redirect('app:show_all')
+    else:
+        return render(request, 'app/quote_form.html', {'form': form})
 
 
 def drop(request, pk):
-    pass
+    to_drop = get_object_or_404(Quote, pk=pk)
+
+    if request.method == 'POST':
+        to_drop.delete()
+        messages.success(request, f'{to_drop.author} quote deleted')
+
+    return render(request, 'app/drop_confirmation.html', {'quote': to_drop})
